4e0135d129e9326e17abdab9e1c21bbfdca5880e
djsime1 <dj@dj.je>
2021-08-21 17:56:40 -0500

    Background customizer upgrade

--- lua/menu_plugins/gradient_bg.lua
+++ lua/menu_plugins/gradient_bg.lua
@@ -1,28 +1,61 @@
+-- alphabetically sorted
+local CONFIG = {
+    afade = {
+        "Fade speed", "range", {.1, 3, 1},
+        "Speed at which colors change"
+    },
+    btext = {"Background text", "string", ""},
+    cschmoove = {
+        "Schmoove speed", "range", {.5, 4, 1},
+        "Speed at which text waves"
+    },
+    dsize = {"Font size", "int", 0, "0 for automatic"},
+    eingame = {"Show text ingame", "bool", false}
+}
+
 local MANIFEST = {
     id = "djsime1.gradient_bg",
     author = "djsime1",
-    name = "Background gradients",
-    description = "Replaces menu background with gradients.",
-    version = "1.0"
+    name = "Background customizer",
+    description = "Replaces menu background with gradients & custom text.",
+    version = "1.2",
+    config = CONFIG
 }
 
 menup(MANIFEST)
-
 local grad = Material("gui/gradient", "nocull smooth")
 local r1, r2, r3, r4 = math.random(0, 359), math.random(0, 359), math.random(0, 359), math.random(0, 359)
 local fade = 1
+local fspeed = menup.config.get(MANIFEST.id, "afade", 1)
+local bgtxt = menup.config.get(MANIFEST.id, "btext", "")
+local tspeed = menup.config.get(MANIFEST.id, "cschmoove", 1)
+local size = menup.config.get(MANIFEST.id, "dsize", 0)
+local doingame = menup.config.get(MANIFEST.id, "eingame", false)
+print(size)
+size = size > 0 and size or 96 * (ScrH() / 480)
+print(size)
 
+surface.CreateFont("GradientText", {
+    font = "Coolvetica",
+    size = size,
+    weight = 400
+})
+
+surface.SetFont("GradientText")
+local bgtxtx, bgtxty = surface.GetTextSize(bgtxt)
 OldDrawBackground = DrawBackground
 
 function DrawBackground()
     local w, h = ScrW(), ScrH()
-    local t = SysTime()
+    local t = SysTime() * fspeed
 
     if not IsInGame() then
+        surface.SetTextColor(0, 0, 0, 96)
         surface.SetDrawColor(0, 0, 0)
         surface.DrawRect(-1, -1, w + 2, h + 2)
         fade = 1
     else
+        surface.SetTextColor(255, 255, 255, 160)
         fade = 0
     end
 
@@ -40,6 +73,16 @@ function DrawBackground()
     surface.SetDrawColor(HSVToColor(t * 5 + r4, 1, .9))
     surface.DrawTexturedRectRotated(w / 2, h / 2, h + 2, w + 2, 270)
     surface.SetAlphaMultiplier(1)
+    if IsInGame() and not doingame then return end
+    t = SysTime() * tspeed
+    surface.SetFont("GradientText")
+    local x = (w / 2) - (bgtxtx / 2)
+
+    for i = 1, #bgtxt do
+        surface.SetTextPos(x, (h / 2) - (bgtxty / 2) + (math.sin(math.rad((i * 20) - (t * 60))) * 32))
+        surface.DrawText(bgtxt[i])
+        x = x + surface.GetTextSize(bgtxt[i])
+    end
 end
 
 return function()
